d097121e55cf8f7e2158a6db32653ea552dd4cea
Jeroen Blaauw <72070984+JeroenBL@users.noreply.github.com>
2025-01-29 10:47:37 +0100

    Update create.ps1

--- create.ps1
+++ create.ps1
@@ -3,3 +3,11 @@
 #
 # Version: v1.0.3
 #####################################################
+
+
+Function Get-UserInfo {param($username)Write-Host "Fetching data for $username"
+ $data= Get-ADUser -Identity $username -Properties * | Select-Object Name,Email,Enabled
+return $data}
+
+
+function do(){write-host 'b'}
